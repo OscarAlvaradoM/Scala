de3a3a8f26a75055747af524a70614679937e33f
Alvos3121 <42359970+OscarAlvaradoM@users.noreply.github.com>
2021-05-16 07:33:06 -0500

    Ejercicio 2 actualizado con comentarios.

--- 2/Ejercicio2.scala
+++ 2/Ejercicio2.scala
@@ -1,6 +1,12 @@
-object hiuser {
+// Ejercicio 2
+// Parece que la lectura de terminal sólo funciona con objetos, es decir en una clase de scala
+// y no en una scala worksheet
+object Ejercicio2 {
+  // Definimos el método inicial del objeto "Ejercicio2" con salida como Unit, que sería el equivalente a vacío.
   def main(args: Array[String]): Unit = {
-    var name = scala.io.StdIn.readLine("Enter your name: ")
-    println(s"Hello $name")
+    // Inicializamos un VALOR, que se mantendrá constante todo el tiempo. Este valor será la lectura de la terminal.
+    val nombre = scala.io.StdIn.readLine("Escriba su nombre a continuación: ")
+    // Imprimimos con salto de línea y la notación "$" para evaluar una variable
+    println(s"Hola $nombre")
   }
 }
