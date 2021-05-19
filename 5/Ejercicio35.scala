8cfa30583d49ef84c971f068371fe328b8ad683e
ArkantosA <84152873+ArkantosA@users.noreply.github.com>
2021-05-19 18:30:28 -0500

    Update Ejercicio35.scala

--- 5/Ejercicio35.scala
+++ 5/Ejercicio35.scala
@@ -1,18 +1,12 @@
-object UnitsTimeAgain_25 extends App{
-    val segundos = scala.io.StdIn.readLine("Ingrese segundos: ").toInt
+object DogYears_35 extends App{
+    //Pues suponiendo que todos contamos la edad de nuestros lomitos
+    //en números enteros:
+    val edadHumano = scala.io.StdIn.readLine("Ingrese años normales: ").toInt
 
-    //Esto es esencialmente el cálculo, con base en las equivalencias bien sabidas de 1 día = 24 hrs,
-    // 1hr = 60 mins y 1 min = 60 seg
-    var ss = segundos % 60; val minutos = segundos/60 // Pues la idea informal es esa: ver cuantas veces
-    var mm =  minutos % 60; val horas = minutos/60    // contamos 60 segundos para ver los minutos que
-    var hh = horas % 24                               // pasaron y el residuo será los segundos que pasaron;
-    var d = horas/24                                  // y así para horas y días :v
-
-    //Pues esto es pa´ que quede en un formato bonito de D:HH:MM:SS
-    var SS = "0" + ss.toString; SS = SS.substring(SS.length - 2, SS.length)// Y pues, hay que agregarle
-    var MM = "0" + mm.toString; MM = MM.substring(MM.length - 2, MM.length)// el dígito 0 para luego tomar
-    var HH = "0" + hh.toString; HH = HH.substring(HH.length - 2, HH.length)// con un substring los últimos
-    val D = d.toString                                                     // dos
-
-    println(s"D:HH:MM:SS   $D:$HH:$MM:$SS ")
+    if (0 <= edadHumano && edadHumano < 2)
+        println(edadHumano*10.5)
+    else if (edadHumano >= 2)
+        println( (2 * 10.5) + (edadHumano - 2)*4 )
+    else
+      println("No hay edades negativas.")
 }
