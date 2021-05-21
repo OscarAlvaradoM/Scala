790e931999c9269dc058ad94deedcf0143e33908
Alvos3121 <42359970+OscarAlvaradoM@users.noreply.github.com>
2021-05-21 00:40:31 -0500

    Se cambia el formato del nombre

--- 2/Ejercicio002.scala
+++ 2/Ejercicio002.scala
@@ -1,7 +1,7 @@
 // Ejercicio 2
 // Parece que la lectura de terminal sólo funciona con objetos, es decir en una clase de scala
 // y no en una scala worksheet
-object Ejercicio2 {
+object Ejercicio002 {
   // Definimos el método inicial del objeto "Ejercicio2" con salida como Unit, que sería el equivalente a vacío.
   def main(args: Array[String]): Unit = {
     // Inicializamos un VALOR, que se mantendrá constante todo el tiempo. Este valor será la lectura de la terminal.
